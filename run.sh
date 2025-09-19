e4bd5176ff229c28ef3cacb4081aafbd41115172
DarkXwasi <wasi24265@gmail.com>
2025-09-19 14:02:55 +0500

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,10 @@
+Create run.sh (make executable chmod +x run.sh):
+
+#!/bin/bash
+clear
+echo "======================================"
+echo "         🔥 DARKXPRIME 🔥"
+echo "======================================"
+git pull 2>/dev/null
+python main.py
+
